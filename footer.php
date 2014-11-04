922042029697d965f3c73be79fd16daf3ab4ce8d
everaldomatias <matiaseveraldo@gmail.com>
2014-11-04 19:04:03 -0200

    Sprint CSS

--- footer.php
+++ footer.php
@@ -24,13 +24,13 @@ $mapa_img = $mapa_img[0];
             <span class="col-md-12">CNPJ <?php echo $footer_opts['footer_cnpj'];?></span>
             <span class="col-md-12"><?php echo $endereco; ?></span>
         </div>
-        <div class="col-md-4 pull-left">
+        <div class="col-md-4 pull-left infos">
         	<span class="tel"><?php echo $footer_opts['footer_tel'];?></span>
         	<?php for ($i = 0; $i < count($emails); $i++): ?>
-        	  <a href="mailto:<?php echo $emails[$i];?>" class="col-md-12 emails"><?php echo $emails[$i]; ?></a>
+        	  <a class="email" href="mailto:<?php echo $emails[$i];?>" class="col-md-12 emails"><?php echo $emails[$i]; ?></a>
             <?php endfor; ?>
         </div><!-- .col-md-3 pull-left -->
-        <a href="<?php echo $footer_opts['footer_mapa_link'];?>" class="col-md-4 pull-right mapa_footer">
+        <a href="<?php echo $footer_opts['footer_mapa_link'];?>" class="col-md-4 pull-right mapa_footer" target="_blank">
         	<img src="<?php echo $mapa_img;?>">
         </a><!-- .col-md-4 mapa_footer -->
     </div>
