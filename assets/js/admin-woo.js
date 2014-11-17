06e7f6da95a30442140e308a101339c6a7b30797
matheus <contato@matheusgimenez.com.br>
2014-11-17 10:47:09 -0200

    removendo algumas tabs

--- assets/js/admin-woo.js
+++ assets/js/admin-woo.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($) {
 	$('.options_group.pricing').css('display','none');
+    $('ul .shipping_tab').css('display','none');
 	$('#_regular_price').attr('value','1');
 });
