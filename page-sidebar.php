9e59b240fa4c0cb96538d6fd4686817af711ee8c
everaldomatias <matiaseveraldo@gmail.com>
2014-11-23 08:30:54 -0200

    Ajustes CSS Contato

--- page-sidebar.php
+++ page-sidebar.php
@@ -1,36 +1,44 @@
 <?php
-/**
- * Template Name: With Sidebar
- *
- * The template for displaying pages with sidebar.
- *
- * @package Odin
- * @since 2.2.0
- */
-
-get_header();
+	get_header('shop');
 ?>
+<div class="col-md-12 clear-mob"></div>
 
-	<div id="primary" class="<?php echo odin_page_sidebar_classes(); ?>">
-		<div id="content" class="site-content" role="main">
+<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+	<header class="col-md-12 wooheader">
+		<div class="col-md-5 pull-left">
+			<h1 class="page-title"><?php the_title(); ?></h1>
+		</div><!-- .col-md-5 pull-left -->
+		<div class="woo-term-maislidos pull-right">
+			<?php do_action('woo_term_hide_mini_query'); ?>
+		</div><!-- .pull-right -->
+	</header><!-- .wooheader -->
+<?php endif; ?>
 
-			<?php
+<div class="col-md-12 clear-mob"></div>
+
+<div class="col-md-3 single" id="sidebar-woo">
+	<?php get_sidebar('woo'); ?>
+</div><!-- #sidebar-woo.col-md-2 -->
+
+<div class="col-md-12 clear-mob"></div>
+
+<div id="primary" class="col-md-8 pull-right single">
+	<div class="col-md-12 clear-mob"></div>
+	<div class="col-md-12" style="clear:both;height:20px;"></div>
+	&nbsp;
+	<div id="content" class="site-content contato" role="main">
+		<?php
 				// Start the Loop.
-				while ( have_posts() ) : the_post();
+		while ( have_posts() ) : the_post();
+			
+		     the_content();
 
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
+		endwhile;
+		?>
+	</div><!-- #content -->
+	<div class="col-md-12 clear-mob"></div>
+</div><!-- #primary -->
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile;
-			?>
+<?php get_template_part('content','bottom'); ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
