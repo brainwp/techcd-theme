163d06920dffd9c8012a4b98e9431a9bf33f02f5
matheus <contato@matheusgimenez.com.br>
2014-12-05 17:19:49 -0200

    issue #35

--- searchform.php
+++ searchform.php
@@ -11,7 +11,13 @@
 		<fieldset>
 			<input type="text" name="s" placeholder="Busca" class="input-text autoclear" />
 			<input type="submit" value="" class="input-submit" />
-			<input type="hidden" name="post_type" value="product" />
+			<?php if(is_post_type_archive('post') || is_singular('post') || is_category() || is_tag() || is_search() && isset($_GET['post_type']) && $_GET['post_type'] == 'post' ): ?>
+				<input type="hidden" name="post_type" value="post" />
+				post
+		    <?php else: ?>
+		        <input type="hidden" name="post_type" value="product" />
+		        woo
+	     	<?php endif; ?>
 		</fieldset>
 	</form>
 </div><!-- .col-md-2 busca_top -->
