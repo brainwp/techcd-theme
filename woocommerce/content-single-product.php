a0a5de9f1ee75600894f1cd8831977b8eee0ed32
everaldomatias <everaldo@brasa.art.br>
2014-12-05 17:05:07 -0200

    Resolve quebra nos downloads da single produtos issue #26

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -26,23 +26,30 @@ if(!empty($slider) && isset($slider)){
 	<?php the_content(); ?>
 </div><!-- #woo-content-single.col-md-9 pull-left -->
 <div class="col-md-12 clear-mob"></div><!-- .col-md-12 clear-mob -->
-<div class="col-md-5 pull-right" id="woo-content-cart">
-	<?php do_action( 'woocommerce_single_product_summary' ); ?>
-</div><!-- #woo-content-cart.col-md-5 pull-right -->
-<div class="col-md-12 clear-mob"></div><!-- .col-md-12 clear-mob -->
-<?php $posts = get_post_meta( get_the_ID(), 'downloads_rel', true ); ?>
-<?php if( $posts ): ?>
-    <div class="col-md-5 content-servicos pull-right" id="woo-content-downloads">
-    	<div class="col-md-12" id="download-ico"></div>
-    	<div class="col-md-12 itens-servicos">
-    		<?php foreach( $posts as $post_id): // variable must be called $post (IMPORTANT) ?>
-    		    <?php $link = get_the_permalink($post_id); ?>
-    		    <?php $title = get_the_title($post_id); ?>
-    		    <?php if($link !== false && $title !== false): ?>
-    		        <a class="col-md-12" href="<?php echo $link; ?>"><?php echo $title; ?></a>
-    		    <?php endif; ?>
-            <?php endforeach; ?>
-        </div><!-- .col-md-12 itens-servicos -->
-    </div><!-- #woo-content-downloads.col-md-5 pull-right -->
-<?php endif; ?>
-<div class="col-md-12 clear-mob"></div><!-- .col-md-12 clear-mob -->
+
+<div class="sidebar-single-produto col-md-5 pull-right">
+
+	<div class="col-md-12" id="woo-content-cart">
+		<?php do_action( 'woocommerce_single_product_summary' ); ?>
+	</div><!-- #woo-content-cart.col-md-5 pull-right -->
+
+	<div class="col-md-12 clear-mob"></div><!-- .col-md-12 clear-mob -->
+
+	<?php $posts = get_post_meta( get_the_ID(), 'downloads_rel', true ); ?>
+	<?php if( $posts ): ?>
+	    <div class="col-md-12 content-servicos" id="woo-content-downloads">
+	    	<div class="col-md-12" id="download-ico"></div>
+	    	<div class="col-md-12 itens-servicos">
+	    		<?php foreach( $posts as $post_id): // variable must be called $post (IMPORTANT) ?>
+	    		    <?php $link = get_the_permalink($post_id); ?>
+	    		    <?php $title = get_the_title($post_id); ?>
+	    		    <?php if($link !== false && $title !== false): ?>
+	    		        <a class="col-md-12" href="<?php echo $link; ?>"><?php echo $title; ?></a>
+	    		    <?php endif; ?>
+	            <?php endforeach; ?>
+	        </div><!-- .col-md-12 itens-servicos -->
+	    </div><!-- #woo-content-downloads.col-md-5 pull-right -->
+	<?php endif; ?>
+
+	<div class="col-md-12 clear-mob"></div><!-- .col-md-12 clear-mob -->
+</div><!-- sidebar-single-produto -->
